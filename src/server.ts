fa159a1d57c74414cd761ec502575661ea396ec5
Md Shafikur Rahaman <58303621+mdshofiur@users.noreply.github.com>
2024-02-12 15:17:57 +0600

    new api route create

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
-import express, { Express } from 'express';
+import express, { Express, Request, Response, NextFunction } from 'express';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import { default as axios } from 'axios';
-import { RequestHandler } from 'express';
+import axios from 'axios';
 import { setupTracing } from './tracer';
+import * as api from '@opentelemetry/api';
 
 setupTracing('example-express-server');
 
@@ -14,8 +14,6 @@ const app: Express = express();
 app.use(cors());
 app.use(cookieParser());
 dotenv.config();
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
    res.send('Hello, World!');
@@ -32,7 +30,7 @@ const getCrudController = () => {
    return router;
 };
 
-const authMiddleware: RequestHandler = (req, res, next) => {
+const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
    const { authorization } = req.headers;
    if (authorization && authorization.includes('secret_token')) {
       next();
@@ -42,28 +40,62 @@ const authMiddleware: RequestHandler = (req, res, next) => {
 };
 
 app.use(express.json());
-app.get('/health', (req, res) => res.status(200).send('HEALTHY')); // endpoint that is called by framework/cluster
-app.get('/run_test', async (req, res) => {
-   // Calls another endpoint of the same API, somewhat mimicing an external API call
-   const createdCat = await axios.post(
-      `http://localhost:${PORT}/cats`,
-      {
-         name: 'Tom',
-         friends: ['Jerry'],
-      },
-      {
-         headers: {
-            Authorization: 'secret_token',
-         },
-      },
-   );
+app.get('/health', (req, res) => res.status(200).send('HEALTHY'));
 
-   return res.status(201).send(createdCat.data);
+app.get('/run_test', async (req, res) => {
+   try {
+      const createdCat = await axios.post(
+         `http://localhost:${PORT}/cats`,
+         {
+            name: 'Tom',
+            friends: ['Jerry'],
+         },
+         {
+            headers: {
+               Authorization: 'secret_token',
+            },
+         },
+      );
+      res.status(201).send(createdCat.data);
+   } catch (error: any) {
+      console.error('Error:', error.message);
+      res.status(500).send('Internal Server Error');
+   }
 });
+
 app.use('/cats', authMiddleware, getCrudController());
 
+const tracer = setupTracing('example-expres');
 
+// Define a route
+app.get('/api/make-request', async (req: Request, res: Response) => {
+   const span = tracer.startSpan('client 55', {
+      kind: api.SpanKind.CLIENT,
+   });
 
+   // const span = api.trace
+   //    .getTracer('example-express')
+   //    .startSpan('client.makeRequest()', {
+   //       kind: api.SpanKind.CLIENT,
+   //    });
+
+   api.context.with(api.trace.setSpan(api.context.active(), span), async () => {
+      try {
+         const response = await axios.get(`http://localhost:${PORT}/run_test`);
+         console.log('status:', response.statusText);
+         span.setStatus({ code: api.SpanStatusCode.OK });
+          span.end();
+      } catch (e: any) {
+         console.error('failed:', e.message);
+         span.setStatus({
+            code: api.SpanStatusCode.ERROR,
+            message: e.message,
+         });
+          span.end();
+      } 
+   });
+   res.send('Request initiated!');
+});
 
 app.listen(PORT, () => {
    console.log(`Listening on http://localhost:${PORT}`);
