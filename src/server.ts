70e83ef885bcc84e6d3c679ce033689aab0b9d68
Md Shafikur Rahaman <58303621+mdshofiur@users.noreply.github.com>
2024-02-14 01:08:53 +0600

    docker-compose and config file created and issue fixed

--- src/server.ts
+++ src/server.ts
@@ -3,98 +3,47 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import axios from 'axios';
+import { api } from '@opentelemetry/sdk-node';
 import { setupTracing } from './tracer';
-import * as api from '@opentelemetry/api';
-
-setupTracing('example-express-server');
 
 const PORT: number = parseInt(process.env.PORT || '8080');
 const app: Express = express();
 
 app.use(cors());
 app.use(cookieParser());
+app.use(express.json());
 dotenv.config();
 
-app.get('/', (req, res) => {
+const tracer = setupTracing('user-fetcher');
+
+app.get('/', (req: Request, res: Response) => {
    res.send('Hello, World!');
 });
 
-const getCrudController = () => {
-   const router = express.Router();
-   const resources: any[] = [];
-   router.get('/', (req, res) => res.send(resources));
-   router.post('/', (req, res) => {
-      resources.push(req.body);
-      return res.status(201).send(req.body);
-   });
-   return router;
-};
+app.get('/user', async (req: Request, res: Response) => {
 
-const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
-   const { authorization } = req.headers;
-   if (authorization && authorization.includes('secret_token')) {
-      next();
-   } else {
-      res.sendStatus(401);
-   }
-};
-
-app.use(express.json());
-app.get('/health', (req, res) => res.status(200).send('HEALTHY'));
-
-app.get('/run_test', async (req, res) => {
-   try {
-      const createdCat = await axios.post(
-         `http://localhost:${PORT}/cats`,
-         {
-            name: 'Tom',
-            friends: ['Jerry'],
-         },
-         {
-            headers: {
-               Authorization: 'secret_token',
-            },
-         },
-      );
-      res.status(201).send(createdCat.data);
-   } catch (error: any) {
-      console.error('Error:', error.message);
-      res.status(500).send('Internal Server Error');
-   }
-});
-
-app.use('/cats', authMiddleware, getCrudController());
-
-const tracer = setupTracing('example-expres');
-
-// Define a route
-app.get('/api/make-request', async (req: Request, res: Response) => {
-   const span = tracer.startSpan('client 55', {
+   const span = tracer.startSpan(`client`, {
       kind: api.SpanKind.CLIENT,
    });
 
-   // const span = api.trace
-   //    .getTracer('example-express')
-   //    .startSpan('client.makeRequest()', {
-   //       kind: api.SpanKind.CLIENT,
-   //    });
-
    api.context.with(api.trace.setSpan(api.context.active(), span), async () => {
       try {
-         const response = await axios.get(`http://localhost:${PORT}/run_test`);
-         console.log('status:', response.statusText);
+         const response = await axios.get(
+            'https://jsonplaceholder.typicode.com/users',
+         );
+         res.json(response.data);
          span.setStatus({ code: api.SpanStatusCode.OK });
-          span.end();
-      } catch (e: any) {
-         console.error('failed:', e.message);
+         // trace.getTracer('user').startSpan('fetching user.....').end();
+      } catch (error: any) {
+         res.status(500).json({ error: error.message });
          span.setStatus({
             code: api.SpanStatusCode.ERROR,
-            message: e.message,
+            message: error.message,
          });
-          span.end();
-      } 
+      } finally {
+         span.end();
+      }
    });
-   res.send('Request initiated!');
 });
 
 app.listen(PORT, () => {
--- src/tracer.ts
+++ src/tracer.ts
@@ -1,123 +1,42 @@
 'use strict';
 
-const opentelemetry = require('@opentelemetry/api');
-import { SpanKind, Attributes } from '@opentelemetry/api';
-import { registerInstrumentations } from '@opentelemetry/instrumentation';
 import { NodeTracerProvider } from '@opentelemetry/sdk-trace-node';
-import {
-   Sampler,
-   AlwaysOnSampler,
-   SimpleSpanProcessor,
-} from '@opentelemetry/sdk-trace-base';
+import { SimpleSpanProcessor } from '@opentelemetry/sdk-trace-base';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-proto';
-import { ZipkinExporter } from '@opentelemetry/exporter-zipkin';
-import { JaegerExporter } from '@opentelemetry/exporter-jaeger';
 import { Resource } from '@opentelemetry/resources';
-import {
-   SemanticAttributes,
-   SemanticResourceAttributes as ResourceAttributesSC,
-} from '@opentelemetry/semantic-conventions';
-import { ExpressInstrumentation } from '@opentelemetry/instrumentation-express';
+import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
+import { trace } from '@opentelemetry/api';
+import { registerInstrumentations } from '@opentelemetry/instrumentation';
 import { HttpInstrumentation } from '@opentelemetry/instrumentation-http';
-// const { HttpInstrumentation } = require('@opentelemetry/instrumentation-http');
+import { ExpressInstrumentation } from '@opentelemetry/instrumentation-express';
+import { MongooseInstrumentation } from '@opentelemetry/instrumentation-mongoose';
 
-
-const { diag, DiagConsoleLogger, DiagLogLevel } = opentelemetry;
-diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.INFO);
-
-
-/* -------------------------------------------------------------------------- */
-/*            Set up the exporter based on the environment variable           */
-/* -------------------------------------------------------------------------- */
-const Exporter = (process.env.EXPORTER || '').toLowerCase().startsWith('z')
-   ? ZipkinExporter
-   : OTLPTraceExporter;
-
-
-/* -------------------------------------------------------------------------- */
-/*                               Set up tracing                               */
-/* -------------------------------------------------------------------------- */
 export const setupTracing = (serviceName: string) => {
    const provider = new NodeTracerProvider({
       resource: new Resource({
-         [ResourceAttributesSC.SERVICE_NAME]: serviceName,
+         [SemanticResourceAttributes.SERVICE_NAME]: serviceName,
       }),
-      sampler: filterSampler(ignoreHealthCheck, new AlwaysOnSampler()),
-   });
-   registerInstrumentations({
-      tracerProvider: provider,
-      instrumentations: [
-         // Express instrumentation expects HTTP layer to be instrumented
-         new HttpInstrumentation(),
-         new ExpressInstrumentation(),
-      ],
    });
 
-   
-   // Initialize the OpenTelemetry APIs to use the NodeTracerProvider bindings
-   provider.register();
-
-   /* -------------------------------------------------------------------------- */
-   /*                              Add OTLP exporter                             */
-   /* -------------------------------------------------------------------------- */
-   const exporter = new Exporter({
-      serviceName,
+   const exporter = new OTLPTraceExporter({
+      url: 'http://localhost:4318/v1/traces',
    });
 
    provider.addSpanProcessor(new SimpleSpanProcessor(exporter));
 
-   /* -------------------------------------------------------------------------- */
-   /*                             Add Jaeger exporter                            */
-   /* -------------------------------------------------------------------------- */
-   const jaegerExporter = new JaegerExporter({
-      serviceName: serviceName,
-      agentHost: 'localhost',
-      agentPort: 16686,
+   provider.register();
+
+   registerInstrumentations({
+      instrumentations: [
+         new HttpInstrumentation(),
+         new ExpressInstrumentation(),
+         new MongooseInstrumentation(),
+      ],
    });
 
-   provider.addSpanProcessor(new SimpleSpanProcessor(jaegerExporter));
-
-   return opentelemetry.trace.getTracer(serviceName);
+   console.log('Tracing initialized');
+   
+    return trace.getTracer(serviceName);
 };
 
-
-
-/* -------------------------------------------------------------------------- */
-/*                            Filter function type                            */
-/* -------------------------------------------------------------------------- */
-type FilterFunction = (
-   spanName: string,
-   spanKind: SpanKind,
-   attributes: Attributes,
-) => boolean;
-
- /* -------------------------------------------------------------------------- */
- /*                           Filter sampler function                          */
- /* -------------------------------------------------------------------------- */
-function filterSampler(filterFn: FilterFunction, parent: Sampler): Sampler {
-   return {
-      shouldSample(ctx, tid, spanName, spanKind, attr, links) {
-         if (!filterFn(spanName, spanKind, attr)) {
-            return { decision: opentelemetry.SamplingDecision.NOT_RECORD };
-         }
-         return parent.shouldSample(ctx, tid, spanName, spanKind, attr, links);
-      },
-      toString() {
-         return `FilterSampler(${parent.toString()})`;
-      },
-   };
-}
-
-/* -------------------------------------------------------------------------- */
-/*                    Ignore health check requests function                   */
-/* -------------------------------------------------------------------------- */
-function ignoreHealthCheck(
-   spanName: string,
-   spanKind: SpanKind,
-   attributes: Attributes,
-) {
-   return (
-      spanKind !== opentelemetry.SpanKind.SERVER ||
-      attributes[SemanticAttributes.HTTP_ROUTE] !== '/health'
-   );
-}
+// trace.getTracer('test').startSpan('test span').end();
